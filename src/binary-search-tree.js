664f59b75839e3324de7509270ed835e78d300c5
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-10-30 19:48:58 +0100

    resolve: binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,94 @@
-const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+function addNode(node, data) {
+  if (!node) return new Node(data);
+
+  if (data < node.data) {
+    node.left = addNode(node.left, data);
+  } else if (data > node.data) {
+    node.right = addNode(node.right, data);
+  }
+
+  return node;
+}
+
+function findNode(node, data) {
+  if (!node) return null;
+
+  if (data === node.data) return node;
+  if (data < node.data) return findNode(node.left, data);
+  return findNode(node.right, data);
+}
+
+function removeNode(node, data) {
+  if (!node) return null;
+
+  if (data < node.data) {
+    node.left = removeNode(node.left, data);
+    return node;
+  } else if (data > node.data) {
+    node.right = removeNode(node.right, data);
+    return node;
+  } else {
+    // найден узел для удаления
+    if (!node.left && !node.right) return null;
+    if (!node.left) return node.right;
+    if (!node.right) return node.left;
+
+    // оба поддерева есть → найти минимум в правом поддереве
+    let minRight = node.right;
+    while (minRight.left) {
+      minRight = minRight.left;
+    }
+
+    node.data = minRight.data;
+    node.right = removeNode(node.right, minRight.data);
+    return node;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.tree = null;
+  }
+
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.tree;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    this.tree = addNode(this.tree, data);
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    return !!this.find(data);
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    return findNode(this.tree, data);
   }
 
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    this.tree = removeNode(this.tree, data);
   }
 
   min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.tree) return null;
+    let node = this.tree;
+    while (node.left) node = node.left;
+    return node.data;
   }
 
   max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.tree) return null;
+    let node = this.tree;
+    while (node.right) node = node.right;
+    return node.data;
   }
 }
 
